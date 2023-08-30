6bf65f149a8187054a1cf20b1b14b34afef1f805
David Arenas <davejs136@gmail.com>
2023-08-30 10:47:57 -0400

    initia commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const info = ref(null);
+
+async function showData() {
+  const { data } = await useFetch("/api/greetings")
+  info.value = data;
+}
+</script>
+
+<template>
+  <div>
+    <button @click="showData">load data</button>
+    <pre v-if="info">{{ info }}</pre>
+  </div>
+</template>
